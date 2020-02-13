8bb9c3453e232441d54dbc5694fcda131c9c3810
nweerakeshara <thnuwanga@gmail.com>
2020-02-14 01:09:20 +0530

    night

---
+++ resources/views/events-template.blade.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        @extends('head')
+
+         @section('title', 'About-us page')
+
+       
+
+    </head>
+    
+    <body>
+        <!--Navigation-->
+            @include('nav')
+        <!--/Navigation-->
+        <header>
+                
+        
+                <!-- Page Header -->
+                <div id="page-header">
+                    <!-- section background -->
+                    <div class="section-bg" style="background-image: url(./img/background-2.jpg);"></div>
+                    <!-- /section background -->
+    
+                    <!-- page header content -->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="header-content">
+                                    <h1>Events Page</h1>
+                                    <ul class="breadcrumb">
+                                        <li><a href="/">Home</a></li>
+                                        <li class="active">Events</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /page header content -->
+                </div>
+                <!-- /Page Header -->
+            
+            <!-- /HEADER -->
+            </header>
+
+            <br><br>
+            @if(count($event1) > 0)
+            @foreach($event1 as $event)
+            <div class="article event-details" style="padding-left:20%; padding-right:20%">
+							<!-- article img -->
+							<div class="article-img">
+								<img src="./img/post-img.jpg" alt="">
+							</div>
+							<!-- article img -->
+
+							<!-- article content -->
+							<div class="article-content">
+								<!-- article title -->
+								<h2 class="article-title">{{$event -> title}}</h2>
+								<!-- /article title -->
+
+								<!-- article meta -->
+								<ul class="article-meta">
+									
+								</ul>
+								<!-- /article meta -->
+
+								<p>{{$event -> body}}</p>
+							</div>
+							<!-- /article content -->
+
+							<!-- event-meta -->
+							<ul class="event-meta">
+								<li><h4>Event Details</h4></li>
+								<li><i class="fa fa-clock-o"></i><strong>Date:</strong> {{$event -> date_time}}</li>
+								<li><i class="fa fa-map-marker"></i><strong>Location:</strong> {{$event -> location}}</li>
+								
+							</ul>
+							<!-- /event-meta -->
+
+							
+						</div>
+                        <!-- /article -->
+                        @endforeach
+                        @else
+                            
+                        @endif
+
+
+
+                        @if(count($event2) > 0)
+            @foreach($event2 as $event)
+            <div class="article event-details" style="padding-left:20%; padding-right:20%">
+							<!-- article img -->
+							<div class="article-img">
+								<img src="./img/post-img.jpg" alt="">
+							</div>
+							<!-- article img -->
+
+							<!-- article content -->
+							<div class="article-content">
+								<!-- article title -->
+								<h2 class="article-title">{{$event -> title}}</h2>
+								<!-- /article title -->
+
+								<!-- article meta -->
+								<ul class="article-meta">
+									
+								</ul>
+								<!-- /article meta -->
+
+								<p>{{$event -> body}}</p>
+							</div>
+							<!-- /article content -->
+
+							<!-- event-meta -->
+							<ul class="event-meta">
+								<li><h4>Event Details</h4></li>
+								<li><i class="fa fa-clock-o"></i><strong>Date:</strong> {{$event -> date_time}}</li>
+								<li><i class="fa fa-map-marker"></i><strong>Location:</strong> {{$event -> location}}</li>
+								
+							</ul>
+							<!-- /event-meta -->
+
+							
+						</div>
+                        <!-- /article -->
+                        @endforeach
+                        @else
+                            
+                        @endif
+                        
+
+
+
+                        @if(count($event3) > 0)
+            @foreach($event3 as $event)
+            <div class="article event-details" style="padding-left:20%; padding-right:20%">
+							<!-- article img -->
+							<div class="article-img">
+								<img src="./img/post-img.jpg" alt="">
+							</div>
+							<!-- article img -->
+
+							<!-- article content -->
+							<div class="article-content">
+								<!-- article title -->
+								<h2 class="article-title">{{$event -> title}}</h2>
+								<!-- /article title -->
+
+								<!-- article meta -->
+								<ul class="article-meta">
+									
+								</ul>
+								<!-- /article meta -->
+
+								<p>{{$event -> body}}</p>
+							</div>
+							<!-- /article content -->
+
+							<!-- event-meta -->
+							<ul class="event-meta">
+								<li><h4>Event Details</h4></li>
+								<li><i class="fa fa-clock-o"></i><strong>Date:</strong> {{$event -> date_time}}</li>
+								<li><i class="fa fa-map-marker"></i><strong>Location:</strong> {{$event -> location}}</li>
+								
+							</ul>
+							<!-- /event-meta -->
+
+							
+						</div>
+                        <!-- /article -->
+                        @endforeach
+                        @else
+                            
+                        @endif
+                        
+
+
+
+                        @if(count($event4) > 0)
+            @foreach($event4 as $event)
+            <div class="article event-details" style="padding-left:20%; padding-right:20%">
+							<!-- article img -->
+							<div class="article-img">
+								<img src="./img/post-img.jpg" alt="">
+							</div>
+							<!-- article img -->
+
+							<!-- article content -->
+							<div class="article-content">
+								<!-- article title -->
+								<h2 class="article-title">{{$event -> title}}</h2>
+								<!-- /article title -->
+
+								<!-- article meta -->
+								<ul class="article-meta">
+									
+								</ul>
+								<!-- /article meta -->
+
+								<p>{{$event -> body}}</p>
+							</div>
+							<!-- /article content -->
+
+							<!-- event-meta -->
+							<ul class="event-meta">
+								<li><h4>Event Details</h4></li>
+								<li><i class="fa fa-clock-o"></i><strong>Date:</strong> {{$event -> date_time}}</li>
+								<li><i class="fa fa-map-marker"></i><strong>Location:</strong> {{$event -> location}}</li>
+								
+							</ul>
+							<!-- /event-meta -->
+
+							
+						</div>
+                        <!-- /article -->
+                        @endforeach
+                        @else
+                            
+                        @endif
+                        
+
+
+                        @if(count($event5) > 0)
+            @foreach($event5 as $event)
+            <div class="article event-details" style="padding-left:20%; padding-right:20%">
+							<!-- article img -->
+							<div class="article-img">
+								<img src="./img/post-img.jpg" alt="">
+							</div>
+							<!-- article img -->
+
+							<!-- article content -->
+							<div class="article-content">
+								<!-- article title -->
+								<h2 class="article-title">{{$event -> title}}</h2>
+								<!-- /article title -->
+
+								<!-- article meta -->
+								<ul class="article-meta">
+									
+								</ul>
+								<!-- /article meta -->
+
+								<p>{{$event -> body}}</p>
+							</div>
+							<!-- /article content -->
+
+							<!-- event-meta -->
+							<ul class="event-meta">
+								<li><h4>Event Details</h4></li>
+								<li><i class="fa fa-clock-o"></i><strong>Date:</strong> {{$event -> date_time}}</li>
+								<li><i class="fa fa-map-marker"></i><strong>Location:</strong> {{$event -> location}}</li>
+								
+							</ul>
+							<!-- /event-meta -->
+
+							
+						</div>
+                        <!-- /article -->
+                        @endforeach
+                        @else
+                            
+                        @endif
+
+                        <h3 class="article-title" style="text-align: center"><a href="/nextevent/{{$eid}}">Next</a></h3>
+                        <h3 class="article-title" style="text-align: center"><a href="/prevevent/{{$eid}}">Prev</a></h3>
+
+        <br><br>
+        <!--Footer-->
+            @include('footer')
+        <!--/Footer-->
+    </body>
+
+</html>
---
+++ resources/views/posts-template.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        @extends('head')
+
+         @section('title', 'About-us page')
+
+         <style>
+
+        #msg, #cnumber, #name,  #email {
+             width: 100%;
+             padding: 12px;
+             border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-top: 6px;
+            margin-bottom: 16px;
+            resize: vertical;
+        }
+
+
+        </style>
+
+    </head>
+    
+    <body>
+        <!--Navigation-->
+            @include('nav')
+        <!--/Navigation-->
+        <header>
+				
+                <!-- Page Header -->
+                <div id="page-header">
+                    <!-- section background -->
+                    <div class="section-bg" style="background-image: url(./img/background-2.jpg);"></div>
+                    <!-- /section background -->
+    
+                    <!-- page header content -->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="header-content">
+                                    <h1>About page</h1>
+                                    <ul class="breadcrumb">
+                                        <li><a href="/">Home</a></li>
+                                        <li class="active">About</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /page header content -->
+                </div>
+                <!-- /Page Header -->
+            
+            <!-- /HEADER -->
+            </header>
+
+            <br><br>
+
+            <div class="container" style="border-radius:5px; background-color: #f2f2f2;  padding: 20px; ">
+            <h3>Contact Form</h3>
+            <br>
+            <form action="/action_page.php">
+            <label for="fname">Name</label>
+            <input type="text" id="name" name="name" placeholder="Your Name" required>
+            <br><br>
+            <label for="lname">Email</label><br>
+            <input type="email" id="email" name="email" placeholder="Your Email" required>   
+	        <br><br>
+    
+            <label for="lname">Contact Number</label><br>
+            <input type="text" id="cnumber" name="cnumber" placeholder="Your Email" required>   
+	        <br><br>
+    
+            <label for="subject">Message</label>
+            <textarea id="msg" name="msg" placeholder="Write something.." required style="height:200px"></textarea>
+
+            <input type="submit" value="Submit" style="background-color: #4CAF50;
+  				color: white;  padding: 12px 20px;  border: none;  border-radius: 4px;
+  				cursor: pointer;">
+            </form>
+            </div>
+
+
+        <br><br>
+        <!--Footer-->
+            @include('footer')
+        <!--/Footer-->
+    </body>
+
+</html>
